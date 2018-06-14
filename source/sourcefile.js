01ddf7843b7d7df9970f69c2d774a5bbd432ed4b
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-06-14 16:09:57 -0500

    Update sourcefile.js

--- source/sourcefile.js
+++ source/sourcefile.js
@@ -5,12 +5,12 @@ const fs = require("fs");
 const client = new Discord.Client();
 const config = require("./config.json");
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
-client.config = config;
+client.prefix = "eme; ";
 ​
-fs.readdir("./modules/runtime/", (err, files) => {
+fs.readdir("./modules/api/system/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
-    const event = require(`./modules/runtime/${file}`);
+    const event = require(`./modules/api/system/${file}`);
     let eventName = file.split(".")[0];
     client.on(eventName, event.bind(null, client));
   });
@@ -26,6 +26,18 @@ fs.readdir("./modules/api/commands/", (err, files) => {
     let commandName = file.split(".")[0];
     console.log(`Attempting to load command ${commandName}`);
     client.commands.set(commandName, props);
+  });
+
+client.syscmds = new Enmap();
+
+fs.readdir("./modules/runtime/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./modules/runtime/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.syscmds.set(commandName, props);
   });
 });
 ​
