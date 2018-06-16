2da7ebdda139cecb160fe51f41a4189055ea3230
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-06-16 06:41:36 -0500

    Update shard-loc.js

--- shard-loc.js
+++ shard-loc.js
@@ -1,8 +1,3 @@
-/*
-    The following code goes into it's own file, and you run this file
-    instead of your main bot file.
-*/
-​
 const Discord = require('discord.js');
 const Manager = new Discord.ShardingManager('./source/sourcefile.js');
 Manager.spawn(12); // This example will spawn 12 shards (30,000 guilds);
