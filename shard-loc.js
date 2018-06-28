3e8c69408d04c2677d4bcb32432e59c23f76ee55
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-06-28 10:15:32 -0500

    Update shard-loc.js

--- shard-loc.js
+++ shard-loc.js
@@ -1,3 +1,3 @@
 const Discord = require('discord.js');
-const Manager = new Discord.ShardingManager('./source/sourcefile.js');
+const Manager = new Discord.ShardingManager('./sourcefile.js');
 Manager.spawn(12); // This example will spawn 12 shards (30,000 guilds);
